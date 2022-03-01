419cdbf5078434bcb19ca186cb30463edcf11b90
SouleyDia <soul@naker.io>
2022-03-01 19:18:27 +0100

    '

--- server/database/db.py
+++ server/database/db.py
@@ -2,7 +2,7 @@
 from pymongo import MongoClient
 # pprint library is used to make the output look more pretty
 from pprint import pprint
-CONNECTION_STRING = "mongodb+srv://souley:soul@cluster0.rsrxk.mongodb.net/projetNosql?retryWrites=true&w=majority"
+CONNECTION_STRING = ''
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
 client = MongoClient(CONNECTION_STRING)
 db=client.admin
