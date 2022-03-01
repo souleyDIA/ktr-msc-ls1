8e4313400ce3da252aa2b76509e38e02a13776d9
SouleyDia <soul@naker.io>
2022-03-01 11:46:55 +0100

    Connect Client to Backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App(){
+  const [data, setProfil] = useState();
+  useEffect(() => {
+    fetch("profil").then(
+      (res) => res.json()
+      ).then((data) => setProfil(data));
+  }, []);
+
+ return  data? (    
+        <div>{data.profil.map(p=>
+          <li key={p}>{p}</li>
+        )}</div>
+    ): <span>loarding...</span>
+      }
 
 export default App;
--- server/server.py
+++ server/server.py
@@ -1,10 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-
-def index():
-   return '<h1>Hello</h1>'
+@app.route('/profil')
+def profil():
+   return {'profil': ['name1', 'name2', 'name3']}
 
 if __name__ == '__main__':
     app.run(debug=True)
