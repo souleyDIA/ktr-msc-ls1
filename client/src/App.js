1cf352957a4579853175e8a928088f0900f6dd9a
SouleyDia <soul@naker.io>
2022-03-02 10:32:04 +0100

    Added Prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,86 @@
 import React, {useEffect, useState} from "react";
 import axios from 'axios';
+import {
+    View,
+    Flex,
+    Form,
+    TextField,
+    Text,
+    Heading,
+    Image,
+    ActionButton,
+} from '@adobe/react-spectrum'
 
-function App(){
-  const dataContent = {
-    'id': 1,
-    "name": 'Dia',
-    "company": "naker",
-    "email": "blabla@gmail.com",
-    "number": '0121548244',
-    "token": '@@@@@à'
+function App() {
+    const dataContent = {
+        name: 'soul',
+        company: 'naker',
+        email: 'blabla@gmail.com',
+        number: '0121548244',
+        token: '@@@@@à',
+    }
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    function createProfil() {
+        axios
+            .post('/profil', dataContent)
+            .then((data) => {
+                localStorage.setItem('token', data.token)
+                setUser(data.token)
+            })
+            .catch((error) => console.log(error))
     }
 
-  const [data, setProfil] = useState();
-  useEffect(() => {
-    axios.post('/profil',  dataContent )
-  .then(response => console.log(response))
-  .catch(error => console.log(error));
-  }, []);
+    return (
+        user && (
+            <View backgroundColor="gray-200" width="100%" height="100%">
+                <View padding="size-3000">
+                    <Flex direction="column" alignItems="center">
+                        <div className="align-height">
+                            <Form width="size-3000" height="100%">
+                                <Flex
+                                    direction="column"
+                                    alignItems="center"
+                                    justifyContent="center"
+                                    alignContent="center"
+                                    gap="size-200"
+                                >
+                                    <Image
+                                        src={require('../chiffumi_logo.png')}
+                                        alt="Logo chiffumi"
+                                    />
+                                    <Heading level={1}>Login</Heading>
 
- return  data? (    
-        <div>{data.map(p=>
-          <li key={p}>{p}</li>
-        )}</div>
-    ): <span>loarding...</span>
-      }
+                                    <TextField
+                                        width="100%"
+                                        key="username"
+                                        placeholder="username"
+                                        value={username}
+                                        onChange={(v) => handleUsername(v)}
+                                    />
+
+                                    <TextField
+                                        width="100%"
+                                        key="password"
+                                        placeholder="password"
+                                        type="password"
+                                        value={password}
+                                        onChange={(v) => handlePassowrd(v)}
+                                    />
+                                </Flex>
+                                <ActionButton onPress={createProfil}>
+                                    <Flex direction="row" gap="size-600">
+                                        <Text> create </Text>
+                                    </Flex>
+                                </ActionButton>
+                            </Form>
+                        </div>
+                    </Flex>
+                </View>
+            </View>
+        )
+    )
+}
 
 export default App;
